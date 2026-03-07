d4ded3c8c2c8d2a5433d5e12a4522f7c1cc28403
Naman Shiroha <namanshiroha@Namans-MacBook-Pro-2.local>
2026-03-07 07:53:14 +0530

    chore(init): scaffold Next.js 14 project with core dependencies

---
+++ folio/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
